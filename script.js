d647e6d546d506a4d0cebed485768f8bb550e8c5
rakibf <rakiboe@gmail.com>
2021-08-21 03:16:53 +0600

    dom-assignment-done

---
+++ script.js
@@ -0,0 +1,69 @@
+// total cost => html element
+const totalCostElement = document.getElementById("total-cost");
+
+// final total => html element
+const finalTotal = document.getElementById("final-total");
+
+
+// select every configuration button and add event listener
+const configurationButtons = document.querySelectorAll(".btn-outline-secondary");
+for (const configurationButton of configurationButtons) {
+    
+    // get specific configuration cost displayed
+    configurationButton.addEventListener("click", function (event) {
+
+        // different configuration cost => html element
+        const memoryCostElement = document.getElementById("memory-cost");
+        const storageCostElement = document.getElementById("storage-cost");
+        const deliveryCostElement = document.getElementById("delivery-cost");
+
+        switch (event.target.innerText) {
+            case "8GB unified memory":
+                memoryCostElement.innerText = 0;
+                break;
+            case "16GB unified memory":
+                memoryCostElement.innerText = 180;
+                break;
+            case "256GB SSD storage":
+                storageCostElement.innerText = 0;
+                break;
+            case "512GB SSD storage":
+                storageCostElement.innerText = 100;
+                break;
+            case "1TB SSD storage":
+                storageCostElement.innerText = 180;
+                break;
+            case "Aug 25 FREE Prime Delivery":
+                deliveryCostElement.innerText = 0;
+                break;
+            case "Aug 21 Delivery Charge $20":
+                deliveryCostElement.innerText = 20;
+                break;
+        }
+
+        // get total cost and final total cost after setting any configuration
+        totalAndFinalTotal();
+    })
+}
+
+// total and final total
+function totalAndFinalTotal() {
+    const configurationCosts = document.querySelectorAll("tbody span");
+    let sum = 0;
+    for (const configurationCost of configurationCosts) {
+        sum += parseFloat(configurationCost.innerText);
+    }
+    totalCostElement.innerText = sum;
+    finalTotal.innerText = sum;
+}
+
+// promo code input and apply button
+const promoInput = document.querySelector("input[type='text']");
+const promoApplyButton = document.querySelector(".btn-danger");
+promoApplyButton.addEventListener("click", function () {
+    if (promoInput.value == "stevekaku") {
+        const twentyPercent = (parseFloat(totalCostElement.innerText) * 20) / 100;
+        finalTotal.innerText = parseFloat(finalTotal.innerText) - twentyPercent;
+    }
+    promoInput.value = "";
+})
